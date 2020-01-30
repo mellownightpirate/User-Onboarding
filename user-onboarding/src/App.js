405162fab377cdf24fc04ef227898217bc7f1776
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-01-30 14:19:50 +0000

    forked project

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Components/Form";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/Components/Form.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import { withFormik, Form, Field, Formik } from 'formik';
+import * as Yup from "yup";
+import axios from axios;
